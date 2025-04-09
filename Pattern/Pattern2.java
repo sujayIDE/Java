ac44aff64be83066bfa2894574ea6d38c3a5cf1a
sujayIDE <156955269+sujayIDE@users.noreply.github.com>
2025-04-10 00:25:05 +0530

    Add files via upload

---
+++ Pattern/Pattern2.java
@@ -0,0 +1,15 @@
+package strivers;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;i>=j;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
